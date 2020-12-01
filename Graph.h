f15518738c1122d3fa07f8f08b61c7f5d2722e81
tyler.kimbell <35345978+tylerkimbell12@users.noreply.github.com>
2020-12-01 14:51:57 -0700

    Add files via upload

---
+++ Graph.h
@@ -0,0 +1,46 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+
+struct vertex;
+
+struct adjVertex{
+    vertex *v;
+    int weight;
+};
+
+struct vertex{
+    std::string name;
+    std::vector<adjVertex> adj;
+    bool visited = false;
+    int distance = 0;
+    vertex *parent = nullptr;
+};
+
+class Graph
+{
+    public:
+        Graph();
+        ~Graph();
+        void addEdge(std::string v1, std::string v2, int weight);
+        void addVertex(std::string name);
+        void displayEdges();
+        int distanceBetweenWords(std::string word1, std::string word2);
+        void assignDistance(string s);
+    protected:
+    private:
+        //vector<edge> edges;
+        std::vector<vertex> vertices;
+        vertex* search(std::string v);
+        vertex* BFS(std::string v1, std::string v2);
+        bool allVisited();
+        vertex* getMinNode();
+        void unassignDistance();
+
+};
+
+#endif // GRAPH_H
---
+++ final.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fstream>
+#include "Stack.h"
+#include "Graph.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    Graph mygwaf;
+    Stack locationStack;
+    string startingLocation;
+    std::string mainMenu = 
+        "======Main Menu=====\n"
+        "1. Set New Starting Location\n"
+        "2. Set Destination and Follow\n"
+        "3. Config\n"
+        "4. AddLocation\n"
+        "5. Exit\n";
+
+    std::string locationList= 
+        "======Avalible Locations=====\n"
+        "Acedemic: C4C, UMC,  Engineering Building, Duane Physics, Math Building,\n"
+        "Fleming Building, Wolf Law Jila, Enviromental Design Building, Econ Building,\n"
+        "Hale Science, Leeds Buisness\n\n"
+        "Recreation: C4C, Fiske Planetarium, Rec Center, Farrand Feild, Kitt Feild, Old Main,\n"
+        "Folsom Feild \n\n"
+        "Residence Halls: Hallet, Engeneering Quad Andrews, Kitt Central, Farrand, Arnett, Baker,\n"
+        "Libby, Sewall, Kitt West, Smith \n";
+
+    int choice;
+    bool exit = false;
+    
+    cout << mainMenu << endl;
+    
+    while(cin >> choice) {
+        
+        // flush the newlines and other characters
+        cin.clear();
+        cin.ignore();
+        
+        switch (choice) {
+            case 1:
+            {
+                cin.clear();
+                cin.ignore();
+                string location;
+                cout << "Choose a starting Location:\n" << locationList << endl;
+                getline(cin, location);
+                startingLocation = location;
+                //TODO
+                //dikjstra using location as root;
+            }
+            
+            case 2:
+            {
+                cin.clear();
+                cin.ignore();
+                string location;
+                cout << "Choose destination starting location is"<< startingLocation <<"\n" << locationList << endl;
+                getline(cin, location);
+                //TODO
+                //find shortest distance to new location and build a stack of nodes to it
+                while(!locationStack.isEmpty()){
+                    //TODO
+                    //Step through locations giving directions
+                }
+            }
+            case 3:
+            {
+                //TODO
+                //implement configurations (mph, descrpitions of buldings, ect.)
+            }
+            case 4:
+            {
+                //TODO
+                //maybe add feature to add locations
+            }
+            case 5:
+            {
+                cout << "Exiting..." << endl;
+                return 0;
+            }
+            default:
+            {
+                cout << "Enter Valid Input" << endl;
+            }
+        
+        if (exit) {
+            break;
+        }
+        
+        cout << mainMenu << endl;
+    }
+    return 0;
+}
